8d82b4f957eee224fe233a662f976f536525b52d
Cesar Diaz <dev.cdiaz@gmail.com>
2023-08-21 15:09:28 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+`use strict`;
+
+// Declaring DOM selectors
+
+const rock = document.getElementById(`rockBtn`);
+const paper = document.getElementById(`paperBtn`);
+const scissors = document.getElementById(`scissorsBtn`);
+
+const score = document.getElementById(`score`);
+
+// Declaring JS only variables
+
+let playerScore = 0;
+let playerSelection = ``;
+let opponentSelection = ``;
+
+// gameState will be the different states the game is in. when initially loading, or when playing again, game state will start at 1. It will become 2 upon selecting rock, paper, or scissors. Game state 3 will be the result screen. Managing game state will make it easier to keep track of dom manipulation.
+let gameState = 1;
+
+// function to generate random 1-3 number to determine computer's selection
+const generateRandomNumber = () => {
+  const min = 1;
+  const max = 3;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// Function to update score
+function updateScore(playerWins) {
+  if (playerWins === true) {
+    playerScore++;
+  } else if (playerWins === false) {
+    playerScore--;
+  }
+  score.innerText = playerScore;
+}
+
+// Starting score
+score.innerText = playerScore;
+
+rock.addEventListener(`click`, function () {
+  playerSelection = `rock`;
+});
+paper.addEventListener(`click`, function () {
+  playerSelection = `paper`;
+});
+scissors.addEventListener(`click`, function () {
+  playerSelection = `scissors`;
+});
